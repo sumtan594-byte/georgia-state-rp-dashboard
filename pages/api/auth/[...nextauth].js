8da589fa99dc8972e438d4e19b13caf883b21a3c
sumtan594-byte <sumtan594@gmail.com>
2026-04-05 19:45:50 +1000

    debug: enable next-auth debug mode

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
 export const authOptions = {
+  debug: true,
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
