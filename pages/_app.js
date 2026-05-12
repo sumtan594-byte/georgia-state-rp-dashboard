0aeaad3e4fb8e2066b788e220bc79a64c66734ce
sumtan594-byte <sumtan594@gmail.com>
2026-05-12 19:22:52 +1000

    add leaflet CSS import for map tiles

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import 'leaflet/dist/leaflet.css';
 import { SessionProvider, useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
 import { useRouter } from 'next/router';
