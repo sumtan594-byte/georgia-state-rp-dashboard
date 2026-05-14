7797007cf6d232a305582ac08c217bca2d922384
sumtan594-byte <sumtan594@gmail.com>
2026-05-14 22:38:46 +1000

    Fix incorrect import path in reminders status API

--- pages/api/panel/reminders/status.js
+++ pages/api/panel/reminders/status.js
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth';
-import { authOptions } from "../../../lib/auth-options";
+import { authOptions } from "../../../../lib/auth-options";
 
 const REMINDERS_ROLE_ID = '1394297547597680670';
 
