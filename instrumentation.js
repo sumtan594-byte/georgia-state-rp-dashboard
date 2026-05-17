dc625ff51edceee2a68a8041667838a7e53118d5
sumtan594-byte <sumtan594@gmail.com>
2026-05-17 22:50:54 +1000

    run reminder worker as standalone process alongside next.js

--- instrumentation.js
+++
@@ -1,8 +0,0 @@
-import { startReminderWorker } from './lib/reminder-worker';
-
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    console.log('[Reminders] Starting reminder worker at server boot...');
-    await startReminderWorker();
-  }
-}
--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    instrumentationHook: true,
-  },
 };
 
 module.exports = nextConfig;
